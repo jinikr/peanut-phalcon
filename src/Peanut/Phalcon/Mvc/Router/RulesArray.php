adb2ea874ff389e25923c320eccc11442dd0a013
Max <smkwon@yellotravel.com>
2016-05-09 00:38:01 +0900

    예제 추가

--- src/Peanut/Phalcon/Mvc/Router/RulesArray.php
+++ src/Peanut/Phalcon/Mvc/Router/RulesArray.php
@@ -115,3 +115,32 @@ class RulesArray extends \Peanut\Phalcon\Mvc\Router
     }
 
 }
+
+/*
+
+routes:
+  param name: \App\Controllers\V1->checkId
+  group {huga:[huga|huga]{4}}:
+    before get|post: \App\Controllers\V1->before
+    after: \App\Controllers\V1->after
+    param name: \App\Controllers\V1->checkId2
+    get {name}: \App\Controllers\V1->getInfo
+    get view/{view_id:[0-9]+}: \App\Controllers\V1->view
+    group bbs:
+      get: \App\Controllers\V1->index
+    group add:
+      get: \App\Controllers\V1->index
+    group {boardId:[0-9]+}:
+      any post|get: \App\Controllers\V1->getNumber
+      group {boardId:[0-9]+}:
+        before: \App\Controllers\V1->index
+        get: \App\Controllers\V1->index
+  group huganew:
+    get: \App\Controllers\V1->index
+  get: /index page
+
+$router = new \Peanut\Phalcon\Mvc\Router\RulesArray();
+$router->group(yaml_parse_file('evn.yaml'));
+return $router;
+
+*/
